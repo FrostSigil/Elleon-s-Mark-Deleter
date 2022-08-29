bc5f42885ede656025297d9fbe122f8dea01933f
FrostSigil <53145101+FrostSigil@users.noreply.github.com>
2022-08-29 20:16:45 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Automatically deletes Elleon's Mark from your inventory when its amount becomes 
 
 ### Proxy commands:
 * **emd** - Toggles the module off/on
+* **emd del 150** - will delete by 150 Elleon's Mark
 
 ### Info:
-Deletes 100 Elleon's Mark when triggered.
+Deletes 100 Elleon's Mark when triggered >1700.
 Удаляет по 100 медалей Эллеона при перевышении количевства в инвентаре 1700шт
 
 ![image](https://teralore.com/items/icon_items/medal2_tex.png)
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict"
 
 const markId = 151643; 
 
@@ -7,19 +8,22 @@ module.exports = function MarkDeleter(mod) {
 	let myGameId = 0n;
 		
     command.add('emd', {
+		"del": arg => {
+			const n = Number(arg);
+			mod.settings.amount = n;
+				mod.command.message(`Delete <font color="#5da8ce">${n}</font>`);			
+		},
         $none() {
             enabled = !enabled;
-			command.message(`Elleon's Mark Deleter is now: ${enabled ? "enabled" : "disabled"}.`);
+			command.message(`Elleon's Mark Deleter : ${enabled ? "enabled" : "disabled"}.`);
 		}
 	});
 	
-	mod.hook('S_LOGIN', mod.majorPatchVersion >= 86 ? 14 : 13, (event) => {
+	mod.hook('S_LOGIN', 14, (event) => {
 		myGameId = event.gameId;
 	})
 	
-	if(mod.majorPatchVersion >= 85)
-	{
-		mod.hook('S_ITEMLIST', mod.majorPatchVersion >= 87 ? 4 : 2, (event) => {
+	mod.hook('S_ITEMLIST', 4, (event) => {
 			if (!enabled) return;
 			
 			for (var i = 0; i < event.items.length; i++)
@@ -30,30 +34,11 @@ module.exports = function MarkDeleter(mod) {
 						gameId: myGameId,
 						pocket: event.pocket,
 						slot: event.items[i].slot,
-						amount: 100
+						amount: mod.settings.amount
 					});
 					break;
 				}
 			}
-		});
-	}
-	else
-	{
-		mod.hook('S_INVEN', 19, (event) => {
-		if (!enabled) return;
-		
-		for (var i = 0; i < event.items.length; i++)
-		{
-			if (event.items[i].id === markId && event.items[i].amount > 1700)
-			{
-				mod.toServer('C_DEL_ITEM', 3, {
-					gameId: myGameId,
-					slot: (event.items[i].slot - 40),
-					amount: 100
-				});
-				break;
-			}
-		}
 	});
-	}
+	
 }
